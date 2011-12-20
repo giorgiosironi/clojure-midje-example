f15a08d25f1bfae9bde043cdf880c2136fa78f1d
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-12-20 08:38:56 +0100

    Leiningen new generated project

---
+++ test/clojure_midje_example/test/core.clj
@@ -0,0 +1,6 @@
+(ns clojure-midje-example.test.core
+  (:use [clojure-midje-example.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
