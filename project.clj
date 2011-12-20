12be36b0d67da1b0db04001d2729d074d62dc88f
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-12-20 09:12:40 +0100

    Modified configuration to grab midje and lein-midje as dependencies

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject clojure-midje-example "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.2.1"]])
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [midje "1.3.0"]]
+  :dev-dependencies [[lein-midje "1.0.7"]])
