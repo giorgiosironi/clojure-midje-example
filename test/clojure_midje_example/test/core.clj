44400c7096f0d8d910984c2b0541b7a289ba28f9
Giorgio Sironi <piccoloprincipeazzurro@gmail.com>
2011-12-20 09:26:50 +0100

    Added a test based on midje

--- test/clojure_midje_example/test/core.clj
+++ test/clojure_midje_example/test/core.clj
@@ -1,6 +1,8 @@
 (ns clojure-midje-example.test.core
   (:use [clojure-midje-example.core])
-  (:use [clojure.test]))
+  (:use [clojure.test])
+  (:use [midje.sweet]))
 
 (deftest the-simplest-test-you-can-write
   (is 3 (+ 1 2)))
+(fact (+ 1 2) => 3)
